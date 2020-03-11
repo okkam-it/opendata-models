602f151c1dc3bcfc7ee5db255ce9b81ec2368f6f
xseris <simone.povoscania@hotmail.it>
2020-03-11 10:26:29 +0100

    Updated SodaResource fields

--- soda/src/main/java/it/okkam/opendata/models/soda/SodaColumnFormat.java
+++ soda/src/main/java/it/okkam/opendata/models/soda/SodaColumnFormat.java
@@ -1,9 +1,22 @@
 package it.okkam.opendata.models.soda;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SodaColumnFormat {
 
+  @JsonIgnore
   String view;
+
+  @JsonIgnore
   String align;
+
+  @JsonIgnore
+  String precisionStyle;
+
+  @JsonIgnore
+  String noCommas;
 
   public String getView() {
     return view;
@@ -19,6 +32,22 @@ public class SodaColumnFormat {
 
   public void setAlign(String align) {
     this.align = align;
+  }
+
+  public String getPrecisionStyle() {
+    return precisionStyle;
+  }
+
+  public void setPrecisionStyle(String precisionStyle) {
+    this.precisionStyle = precisionStyle;
+  }
+
+  public String getNoCommas() {
+    return noCommas;
+  }
+
+  public void setNoCommas(String noCommas) {
+    this.noCommas = noCommas;
   }
 
 }
